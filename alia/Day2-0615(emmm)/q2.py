b516f941c6d1a29e3bcf5b0238f681f6bdb8062d
AliaCai <Caiheyuncoco@outlook.com>
2025-06-15 17:45:30 -0400

    uknow what, let me use c++ in the future

---
+++ alia/Day2-0615(emmm)/q2.py
@@ -0,0 +1,31 @@
+#11. Container With Most Water: https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=top-100-liked
+#worst time efficiency
+# class Solution:
+#     def maxArea(self, height: List[int]) -> int:
+#         max=0;
+#         for f in range (0, len(height)-1):
+#             for t in range (1, len(height)):
+#                 if ((t-f)*min(height[f], height[t]))>max:
+#                     max=(t-f)*min(height[f], height[t])
+
+
+#         return max;
+
+
+# 2 pointers issue
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left=0
+        right =len(height)-1
+        max=0
+        size=0
+        while left<=right:
+            size=(right-left)*min(height[right], height[left])
+            if(size>max):
+                max=size
+            
+
+            if(height[right]>height[left]):
+                left=left+1;
+            else:
+                right=right-1;
